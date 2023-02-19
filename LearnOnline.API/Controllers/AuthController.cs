295223d0ba8a77945151ebc3dc58a9ee006bb1ba
Dung Bui <37134075+tiendungcmd@users.noreply.github.com>
2023-02-19 20:56:57 +0700

    update authentication

--- LearnOnline.API/Controllers/AuthController.cs
+++ LearnOnline.API/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
-﻿using LearnOnline.API.Services.AuthService;
+﻿using LearnOnline.API.Data;
+using LearnOnline.API.Services.AuthService;
 using LearnOnline.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LearnOnline.API.Controllers
@@ -12,11 +14,13 @@ namespace LearnOnline.API.Controllers
     {
         private readonly IAuthService _authService;
         private readonly IConfiguration _configuration;
+        private readonly LearnOnlineDbContext _learnOnlineDb;
 
-        public AuthController(IAuthService authService, IConfiguration configuration)
+        public AuthController(IAuthService authService, IConfiguration configuration, LearnOnlineDbContext learnOnlineDb)
         {
             _authService = authService;
             _configuration = configuration;
+            _learnOnlineDb = learnOnlineDb;
         }
         [HttpPost("register")]
         public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegister request)
@@ -40,6 +44,21 @@ namespace LearnOnline.API.Controllers
         public async Task<ActionResult<ServiceResponse<string>>> Login(UserLogin request)
         {
             var response = await _authService.Login(request.Email, request.Password);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("change-password")]
+        public async Task<ActionResult<ServiceResponse<bool>>> ChangePassword(UserChangePassword newPassword)
+        {
+            var email = newPassword.UserName + "@gmail.com";
+            var user = _learnOnlineDb.Users.FirstOrDefault(x => x.Email == email);
+            var response = await _authService.ChangePassword(user.Id, newPassword.Password);
+
             if (!response.Success)
             {
                 return BadRequest(response);
---
+++ LearnOnline.Models/UserChangePassword.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnOnline.Models
+{
+    public class UserChangePassword
+    {
+        public string UserName { get; set; }
+        [Required, StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+        [Compare("Password", ErrorMessage = "The passwords do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
